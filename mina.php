04c4364cd209dc1c844864db93b81b8c1611a50d
Ott117 <33862281+Ott117@users.noreply.github.com>
2018-01-16 00:03:17 +0200

    minu nimi on Ott

---
+++ mina.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 96hro
+ * Date: 11.12.2017
+ * Time: 12:11
+ */
+// üherealine kommentaar
+/*
+ * mitme
+ * realine
+ * kommentaar
+ */
+/*
+ * Defineeri oma nimega muutuja,
+ * mille väärtuseks on Sinu eesnimi
+ * ja väljasta antud muutuja väärtus
+ * ekraani koos lisatekstiga
+ * näiteks:
+ * Minu nimi on Eesnimi*/
+$anna = 'Ott';
+// echo käsuga
+echo 'echo: Minu nimi on '.$Ott.'<br />';
+// print funktisooniga
+print 'print: Minu nimi on '.$Ott.'<br />';
+print("print: Minu nimi on $Ott<br />");
