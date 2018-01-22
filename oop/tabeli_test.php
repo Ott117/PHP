35154fc5b2c5b3dc39497a1ecc455f71de71d238
Ott117 <33862281+Ott117@users.noreply.github.com>
2018-01-22 22:25:56 +0200

    tabeli yl

--- oop/tabeli_test.php
+++ oop/tabeli_test.php
@@ -2,9 +2,11 @@
 /**
  * Created by PhpStorm.
  * User: 96hro
- * Date: 15/01/2018
- * Time: 23:53
+ * Date: 22/01/2018
+ * Time: 22:25
  */
+
+
 require_once 'tabel.php';
 // loome tabel kirjelduse järgi tabeli objekt
 $minuTabel = new tabel(array('a', 'b', 'c'));
@@ -17,3 +19,15 @@ echo '<pre>';
 print_r($minuTabel);
 echo '</pre>';
 $minuTabel->prindiTabel();
+require_once 'htmlTabel.php';
+// loome tabel kirjelduse järgi tabeli objekt
+$htmlTabel = new htmlTabel(array('a', 'b', 'c'), '#FF0000');
+// lisame tabeli read
+$htmlTabel->lisaRida(array(1, 2, 3));
+$htmlTabel->lisaRida(array(4, 5, 6));
+$htmlTabel->lisaReaKirjeldusega(array('c'=>7, 'a'=>8, 'b'=>9));
+// väljastame tabel test kujul
+echo '<pre>';
+print_r($htmlTabel);
+echo '</pre>';
+$htmlTabel->prindiTabel();
